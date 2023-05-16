df5f79dcf10d6a7e8bb91cd0787185e0841bce2f
jsdrever <jsdrever09@gmail.com>
2023-05-15 20:42:47 -0500

    webpack plugin and serviceworker dot js

--- client/src/sw.js
+++ client/src/sw.js
@@ -1,51 +1,27 @@
-// Vanilla service worker (no workbox)
-const CACHE_NAME = 'cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/index.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
-  '/images/icon.png',
-];
+// Caching js and css requires workbox-strategies to be installed
+// To actually respond to requests with a cached response, we need to use a strategy called StaleWhileRevalidate
+// This strategy will first check the cache for a response, and if it finds one, it will return it.
 
-// The three phases of the service worker's life cycle are:
-// 1. install
-// 2. activate
-// 3. claim
+const { StaleWhileRevalidate } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
-// Install - the service worker is first installed and then activated.
-self.addEventListener('install', (e) =>
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  )
-);
+// The precacheAndRoute() method takes an array of URLs to precache. The self._WB_MANIFEST is an array that contains the list of URLs to precache.
+precacheAndRoute(self.__WB_MANIFEST);
 
-// Activate - the service worker is activated after install.
-self.addEventListener('activate', (e) =>
-  e.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  )
-);
-
-// Claim - the service worker is claimed after install.
-// When a service worker is initially registered, pages won't use it until the next load.
-// The clients.claim() method is used to claim the service worker immediately.
-self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
-});
-
-// Example of a simple cache-first network-first strategy
-// The service worker is checking the cache for a response and if it doesn't find it, it fetches it.
-self.addEventListener('fetch', (e) =>
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)))
+// Set up asset cache
+registerRoute(
+  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
+    plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
 );
