8477504e5a4138dbdc82913d70d8943c78ad5add
Andrew Allemond <118858494+aallemond@users.noreply.github.com>
2023-05-17 21:41:34 -0500

    fixed issue wheretailwind wasnt being applied

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
     return (
-      <header className="bg-gray-900 text-white py-4">
+      <header className="bg-gray-900 text-white py-4 text-center">
         <div className="container mx-auto">
           <h1 className="text-3xl font-semibold">Senate Explorer</h1>
         </div>
