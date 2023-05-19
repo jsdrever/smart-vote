904e668a6b06421c3ef0105cdbbfc6015b155b91
Andrew Allemond <118858494+aallemond@users.noreply.github.com>
2023-05-18 21:30:27 -0500

    tweaked styling of senators page and added border to navbar

--- client/src/components/pages/Senators.js
+++ client/src/components/pages/Senators.js
@@ -4,17 +4,17 @@ import React, { useState } from 'react';
 const Senators = () => {
   return (
     <body>
-
+    <div className='grid grid-rows-10 grid-cols-5 m-5'>
    <div className='max-w-sm rounded shadow-md border-4 border-red-800 my-5 ml-5 mb-5'>
-    <h1 className='text-blue-900 text-decoration-line: underline font-extrabold text-center'>Alabama</h1>
+    <h1 className='text-blue-900 text-decoration-line: underline font-extrabold text-center font-serif'>Alabama</h1>
     <ul className='text-red-800 text-center'>
       <li><a href=''>Senator Name</a></li>
       <li><a href=''>Senator Name</a></li>
     </ul>
    </div>
 
-   <div className='max-w-sm rounded shadow-md border-4 border-red-800 ml-5 mb-5'>
-    <h1 className='text-blue-900 text-decoration-line: underline font-extrabold  ml-5 text-center'>Alaska</h1>
+   <div className='max-w-sm rounded shadow-md border-4 border-red-800 my-5 ml-5 mb-5'>
+    <h1 className='text-blue-900 text-decoration-line: underline font-extrabold  ml-5 text-center font-serif'>Alaska</h1>
     <div >
     <ul className='text-red-800 text-center '>
       <li><a href=''>Senator Name</a> </li>
@@ -23,37 +23,53 @@ const Senators = () => {
     </div>
    </div>
 
-   <div className='max-w-sm rounded shadow-md border-4 border-red-800 ml-5 mb-5'>
-    <h1 className='text-blue-900 text-decoration-line: underline font-extrabold ml-5 text-center'>Arizona</h1>
+   <div className='max-w-sm rounded shadow-md border-4 border-red-800 my-5 ml-5 mb-5'>
+    <h1 className='text-blue-900 text-decoration-line: underline font-extrabold ml-5 text-center font-serif'>Arizona</h1>
     <ul className='text-red-800 text-center'>
       <li>Senator Name</li>
       <li>Senator Name</li>
     </ul>
    </div>
 
-   <div className='max-w-sm rounded shadow-md border-4 border-red-800 ml-5 mb-5'>
-    <h1 className='text-blue-900 text-decoration-line: underline font-extrabold text-center'>Arkansas</h1>
+   <div className='max-w-sm rounded shadow-md border-4 border-red-800 my-5 ml-5 mb-5'>
+    <h1 className='text-blue-900 text-decoration-line: underline font-extrabold text-center font-serif'>Arkansas</h1>
     <ul className='text-red-800 text-center'>
       <li>Senator Name</li>
       <li>Senator Name</li>
     </ul>
    </div>
 
-   <div className='max-w-sm rounded shadow-md border-4 border-red-800 ml-5 mb-5'>
-    <h1 className='text-blue-900 text-decoration-line: underline font-extrabold text-center'>Alaska</h1>
+   <div className='max-w-sm rounded shadow-md border-4 border-red-800 my-5 ml-5 mb-5'>
+    <h1 className='text-blue-900 text-decoration-line: underline font-extrabold text-center font-serif'>Alaska</h1>
     <ul className='text-red-800 text-center'>
       <li>Senator Name</li>
       <li>Senator Name</li>
     </ul>
    </div>
 
-   <div className='max-w-sm rounded shadow-md border-4 border-red-800 ml-5 mb-5'>
-    <h1 className='mb-4 text-blue-900 text-decoration-line: underline font-extrabold text-center'>California</h1>
+
+   <div className='max-w-sm rounded shadow-md border-4 border-red-800 my-5 ml-5 mb-5'>
+    <h1 className='text-blue-900 text-decoration-line: underline font-extrabold text-center font-serif'>California</h1>
     <ul className='text-red-800 text-center'>
       <li>Senator Name</li>
       <li>Senator Name</li>
     </ul>
    </div>
+
+
+   <div className='max-w-sm rounded shadow-md border-4 border-red-800 my-5 ml-5 mb-5'>
+    <h1 className='text-blue-900 text-decoration-line: underline font-extrabold text-center font-serif'>Colorado</h1>
+    <ul className='text-red-800 text-center'>
+      <li>Senator Name</li>
+      <li>Senator Name</li>
+    </ul>
+   </div>
+
+   </div>
+
+  
+
+   
    
 
     </body>
