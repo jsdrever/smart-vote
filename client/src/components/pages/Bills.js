af8e810c58d168bb49f7307c7f02751015367e74
jsdrever <jsdrever09@gmail.com>
2023-05-20 23:53:32 -0500

    styled the bills page it looks nicer now and set up where we can hopefully get a picture of the senator

--- client/src/components/pages/Bills.js
+++ client/src/components/pages/Bills.js
@@ -12,8 +12,9 @@ const Bills = () => {
         <div className="bg-blue-900 bg-opacity-75 p-8">
         <div className='grid grid-rows-8 grid-cols-1 m-5'>
          <div className='max-w-sm rounded shadow-md border-4 border-red-800 my-5 ml-5 mb-5'>
-          <h1 className='text-blue-900 text-decoration-line: underline font-extrabold text-center font-serif'>Senator Name</h1>
-           <ul className='space-y-4 text-red-800'>
+          <img src='../../../public/images/senate_chamber.jpeg' />
+          <h1 className="text-3xl font-bold text-white mt-10 text-decoration-line: underline font-extrabold text-center font-serif">Senator Name</h1>
+           <ul className="text-lg text-white">
             <li> Current term </li>
             <li> Party Affiliation </li>
            </ul>
@@ -22,29 +23,29 @@ const Bills = () => {
        <div>
         <div className='grid grid-rows-10 grid-cols-4 m-5'>
          <div className='max-w-sm rounded shadow-md border-4 border-red-800 my-5 ml-5 mb-5'>
-          <h1 className='text-blue-900 text-decoration-line: underline font-extrabold text-center font-serif'>Sponsored Bills</h1>
-           <ul className='space-y-4 text-red-800'>
+          <h1 className="text-3xl font-bold text-white mt-10 text-decoration-line: underline font-extrabold text-center font-serif">Sponsored Bills</h1>
+           <ul className="text-lg text-white">
             <li><a href=''>Bill Name</a>  </li>
             <li> A brief description of the bill </li>
            </ul>
        </div>
        <div className='max-w-sm rounded shadow-md border-4 border-red-800 my-5 ml-5 mb-5'>
-        <h1 className='text-blue-900 text-decoration-line: underline font-extrabold text-center font-serif'>Co-Sponsored Bills</h1>
-         <ul className='space-y-4 text-red-800'>
+        <h1 className="text-3xl font-bold text-white mt-10 text-decoration-line: underline font-extrabold text-center font-serif">Co-Sponsored Bills</h1>
+         <ul className="text-lg text-white">
            <li><a href=''>Bill Name</a>  </li>
            <li> A brief description of the bill </li>
          </ul>
        </div>
        <div className='max-w-sm rounded shadow-md border-4 border-red-800 my-5 ml-5 mb-5'>
-        <h1 className='text-blue-900 text-decoration-line: underline font-extrabold text-center font-serif'>Yea</h1>
-         <ul className='space-y-4 text-red-800'>
+        <h1 className="text-3xl font-bold text-white mt-10 text-decoration-line: underline font-extrabold text-center font-serif">Yea</h1>
+         <ul className="text-lg text-white">
            <li><a href=''>Bill Name</a>  </li>
            <li> A brief description of the bill </li>
          </ul>
        </div>
         <div className='max-w-sm rounded shadow-md border-4 border-red-800 my-5 ml-5 mb-5'>
-         <h1 className='text-blue-900 text-decoration-line: underline font-extrabold text-center font-serif'>Nay</h1>
-          <ul className='space-y-4 text-red-800'>
+         <h1 className="text-3xl font-bold text-white mt-10 text-decoration-line: underline font-extrabold text-center font-serif">Nay</h1>
+          <ul className="text-lg text-white">
            <li><a href=''>Bill Name</a>  </li>
            <li> A brief description of the bill </li>
           </ul>
